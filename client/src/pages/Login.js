e71ef007ea7c1edd9e4ee27f7ab34c614f58ab39
Quentin <q.lambkin@gmail.com>
2023-02-17 12:34:08 -0500

    Move log in button

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -47,7 +47,6 @@ const LOGIN_URI =
 const Login = () => (
   <>
     <StyledLoginContainer>
-      <StyledLoginButton href={LOGIN_URI}>Log in to Spotify</StyledLoginButton>
       <StyledDescription>
         Due to the restrictions of Spotify's development mode, you may not be
         able to access your account if you have not previously been granted
@@ -60,6 +59,7 @@ const Login = () => (
         To request access to your account, please send your spotify email to
         q.lambkin@gmail.com
       </StyledDescription>
+      <StyledLoginButton href={LOGIN_URI}>Log in to Spotify</StyledLoginButton>
     </StyledLoginContainer>
   </>
 );
